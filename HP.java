9f60722bc27d4de7640b5d9eabe4053202fdcccd
Sebastian Jörz <sjoerz@skyface.de>
2023-05-10 19:12:43 +0200

    Not using maze anymore (for fitness)

--- HP.java
+++ HP.java
@@ -39,7 +39,6 @@ public class HP {
 
     public void test() {
         population.creatTestPop();
-        population.createMazeFromPopulation();
         population.printModel();
     }
 }
@@ -52,12 +51,14 @@ class Population {
     }
 
     public void creatTestPop() {
-        String[] sequenzes = { "HFPRPRHFPRHF", // Own Example = -2; 0 Overlaps
+        String[] sequenzes = {
+                "HFPRPRHFPRHF", // Own Example = -2; 0 Overlaps
                 "HFPRHFPRPRHLHLPRHFPLPRHLPRHLHLPRPRHFPRHF", // GA01 Example 1 = -4; 0 Overlaps
                 "HFPRHFPRPRHLHLPRHFPRPRHLPRHLHLPRPRHFPRHF", // GA01 Example 2 = -9; 0 Overlaps
-                "HLPLPFPLHLHRPRHF", // GAP00 Praktikum Faltung 1 = -3; 0 Overlaps
-                "HFPLHLHFPRPRPRHF", // GAP00 Praktikum Faltung 2 = -2; 1 Overlap
-                "PFPRPRPR" // Test = 0; 0 Overlaps
+                "HLPLPFPLHLHRPRHF", // GAP00 Praktikum Faltung 1 = -2; 0 Overlaps
+                "HFPLHLHFPRPRPRHF", // GAP00 Praktikum Faltung 2 = -3; 1 Overlap
+                "PFPRPRPR", // Test = 0; 0 Overlaps
+                "HFHRHRHRHRHRHRHRHFHLHLHLHF" // Test = -22; 9 Overlaps
         };
         for (String sequenz : sequenzes) {
             HPModell hpModell = new HPModell();
@@ -80,16 +81,10 @@ class Population {
         }
     }
 
-    public void createMazeFromPopulation() {
-        for (HPModell hpModell : this.hpModellPopulation) {
-            hpModell.createMazeFromPopulation();
-        }
-    }
-
     public void printModel() {
         for (HPModell hpModell : this.hpModellPopulation) {
             // hpModell.printPopulation();
-            hpModell.printMaze();
+            hpModell.printOverlappingMaze();
             hpModell.calcFitness();
             System.out.println("HydroContacts: " + hpModell.getHydroContacts());
             System.out.println("Overlaps: " + hpModell.getOverlaps());
@@ -101,7 +96,7 @@ class Population {
 
 class HPModell {
     private ArrayList<Node> proteins;
-    private int[][] maze = new int[20][20];
+    // private int[][] maze = new int[20][20];
     private int hydroContacts;
     private int overlaps;
 
@@ -156,19 +151,20 @@ class HPModell {
         }
     }
 
-    public void createMazeFromPopulation() {
+    private int[][] createOverlappingMaze() {
         int x = 10;
         int y = 10;
-        maze = new int[20][20];
+        int maze[][] = new int[20][20];
         for (int i = 0; i < 20; i++) {
-            Arrays.fill(maze[i], -1);
+            Arrays.fill(maze[i], 0);
         }
 
         H_Richtung lastH_Richtung = H_Richtung.Nord; // Starting direction
 
         for (Node currentNode : this.proteins) {
 
-            maze[x][y] = currentNode.getIsHydrophobic() ? 1 : 0;
+            // maze[x][y] = currentNode.getIsHydrophobic() ? 1 : 0;
+            maze[x][y] = maze[x][y] + 1;
             currentNode.setX(x);
             currentNode.setY(y);
 
@@ -176,7 +172,8 @@ class HPModell {
             int relRichtung = currentNode.getDirection().getValue();
             int intFromEnums = initRichtung + relRichtung;
             int intFromEnumsMod = intFromEnums % 4;
-            lastH_Richtung = H_Richtung.values()[intFromEnumsMod < 0 ? intFromEnumsMod + 4 : intFromEnumsMod];
+            lastH_Richtung = H_Richtung.values()[intFromEnumsMod < 0 ? intFromEnumsMod +
+                    4 : intFromEnumsMod];
             // Update x and y
             switch (lastH_Richtung) {
                 case Nord:
@@ -196,31 +193,30 @@ class HPModell {
             continue;
 
         }
+        return maze;
+
     }
 
     public void calcFitness() {
 
         this.hydroContacts = 0;
         int firstThree = 3;
-        int x = 10;
-        int y = 10;
-        maze = new int[20][20];
-        for (int i = 0; i < 20; i++) {
-            Arrays.fill(maze[i], -1);
-        }
-        int lastX = x;
-        int lastY = y;
+        int x = 0;
+        int y = 0;
+        // // maze = new int[20][20];
+        // for (int i = 0; i < 20; i++) {
+        // Arrays.fill(maze[i], -1);
+        // }
+        // int lastX = x;
+        // int lastY = y;
 
         H_Richtung lastH_Richtung = H_Richtung.Nord; // Starting direction
         overlaps = 0;
 
-        for (Node currentNode : this.proteins) {
-            // Check overlap
-            if (maze[x][y] == 1) {
-                this.overlaps++;
-            }
+        for (int i = 0; i < this.proteins.size(); i++) {
+            Node currentNode = this.proteins.get(i);
 
-            maze[x][y] = currentNode.getIsHydrophobic() ? 1 : 0;
+            // maze[x][y] = currentNode.getIsHydrophobic() ? 1 : 0;
             currentNode.setX(x);
             currentNode.setY(y);
 
@@ -230,25 +226,28 @@ class HPModell {
             int intFromEnumsMod = intFromEnums % 4;
             lastH_Richtung = H_Richtung.values()[intFromEnumsMod < 0 ? intFromEnumsMod + 4 : intFromEnumsMod];
 
-            if (firstThree <= 0 && currentNode.getIsHydrophobic()) {
-                // Check if left, right, up or down is hydrophobic (without the cords from the
-                // last node)
-                int left = maze[currentNode.getX()][currentNode.getY() - 1];
-                int right = maze[currentNode.getX()][currentNode.getY() + 1];
-                int up = maze[currentNode.getX() - 1][currentNode.getY()];
-                int down = maze[currentNode.getX() + 1][currentNode.getY()];
-
-                if (left == 1 && lastY != currentNode.getY() - 1) {
-                    this.hydroContacts++;
-                }
-                if (right == 1 && lastY != currentNode.getY() + 1) {
-                    this.hydroContacts++;
-                }
-                if (up == 1 && lastX != currentNode.getX() - 1) {
-                    this.hydroContacts++;
-                }
-                if (down == 1 && lastX != currentNode.getX() + 1) {
-                    this.hydroContacts++;
+            if (firstThree <= 0) { // Less than 3 nodes => No hydrophobic contacts and no overlaps
+                for (int j = 0; j < i - 1; j++) // j < i-1 => Excludes self and last
+                {
+                    Node otherNode = this.proteins.get(j);
+                    int otherX = otherNode.getX();
+                    int otherY = otherNode.getY();
+                    if (otherX == x && otherY == y) { // Overlap
+                        overlaps++;
+                    } else if (currentNode.getIsHydrophobic() && otherNode.getIsHydrophobic()) { // No overlap &&
+                                                                                                 // currentNode is
+                                                                                                 // hydrophobic => Check
+                        // for hydrophobic contacts
+                        if (otherX == x && otherY == y - 1) { // Left
+                            this.hydroContacts++;
+                        } else if (otherX == x && otherY == y + 1) { // Right
+                            this.hydroContacts++;
+                        } else if (otherX == x - 1 && otherY == y) { // Up
+                            this.hydroContacts++;
+                        } else if (otherX == x + 1 && otherY == y) { // Down
+                            this.hydroContacts++;
+                        }
+                    }
                 }
             }
 
@@ -268,8 +267,8 @@ class HPModell {
                     break;
             }
 
-            lastX = currentNode.getX();
-            lastY = currentNode.getY();
+            // lastX = currentNode.getX();
+            // lastY = currentNode.getY();
             firstThree--;
             continue;
 
@@ -292,13 +291,12 @@ class HPModell {
         return this.hydroContacts;
     }
 
-    public void printMaze() {
+    public void printOverlappingMaze() {
+        int maze[][] = this.createOverlappingMaze();
         for (int i = 0; i < maze.length; i++) {
             System.out.print("|");
             for (int j = 0; j < maze[i].length; j++) {
-                System.out.print(maze[i][j] == 1 ? "H "
-                        : maze[i][j] == 0 ? "P "
-                                : "  ");
+                System.out.print(maze[i][j] != 0 ? maze[i][j] : " ");
                 System.out.print("|");
             }
             System.out.println();
